87ed2a8f5197f8d9a355018b1603c6fe16f2f924
Dominique Eckert <Dominique.Eckert@unige.ch>
2019-02-22 13:28:39 +0100

    Test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+      name='pyproffit',    # This is the name of your PyPI-package.
+      version='0.1',
+      description='Python package for PROFFIT',
+      author='Dominique Eckert',
+      author_email='Dominique.Eckert@unige.ch',
+      url='http://www.isdc.unige.ch/~deckert/newsite/Proffit.html',
+      py_modules=['pyproffit','miscellaneous','data'],
+)
+

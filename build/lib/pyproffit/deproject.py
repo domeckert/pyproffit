a219febe40dea56bf2566a0cc56ca2a2839b3536
vittoriodx7 <vercan90l19>
2019-11-19 10:10:33 +0100

    fixed bug in plot production+plot_arcmin as well as kpc

--- build/lib/pyproffit/deproject.py
+++ build/lib/pyproffit/deproject.py
@@ -934,7 +934,7 @@ class Deproject:
 
         return mg,mgl,mgh
 
-    def PlotMgas(self,rout=None,outfile=None):
+    def PlotMgas(self,rout=None,outfile=None,do_both=False):
         if self.samples is None or self.z is None or self.cf is None:
             print('Error: no gas density profile found')
             return
@@ -983,22 +983,38 @@ class Deproject:
         ax=fig.add_subplot(111)
 
 
-        ax.plot(rout, mg, color='C0', lw=2, label='Gas mass')
-        ax.fill_between(rout, mgl, mgh, color='C0', alpha=0.5)
+        ax.plot(rkpc, mg, color='C0', lw=2, label='Gas mass')
+        ax.fill_between(rkpc, mgl, mgh, color='C0', alpha=0.5)
 
 
         ax.set_xscale('log')
         ax.set_yscale('log')
         ax.set_ylabel('$M_{gas} [M_\odot]$', fontsize=40)
-        ax.set_xlabel('Radius [arcmin]', fontsize=40)
+        ax.set_xlabel('Radius [kpc]', fontsize=40)
 
         ax.legend(loc=0)
 
         ax.minorticks_on()
         ax.tick_params(length=20, width=1, which='major', direction='in', right='on', top='on')
         ax.tick_params(length=10, width=1, which='minor', direction='in', right='on', top='on')
+
         for item in (ax.get_xticklabels() + ax.get_yticklabels()):
             item.set_fontsize(18)
+
+        if do_both == True:
+            limx=ax.get_xlim()
+            ax2 = ax.twiny()
+            print(limx)
+            print(limx[0]/kpcp)
+            ax2.set_xlim([limx[0]/ kpcp,limx[1]/ kpcp])
+            ax2.set_xscale('log')
+            ax2.tick_params(length=20, width=1, which='major', direction='in', right='on', top='on')
+            ax2.tick_params(length=10, width=1, which='minor', direction='in', right='on', top='on')
+            ax2.set_xlabel('Radius [arcmin]', fontsize=40)
+            for item in (ax2.get_xticklabels() + ax2.get_yticklabels()):
+                item.set_fontsize(18)
+
+
         if outfile is not None:
             plt.savefig(outfile)
             plt.close()
